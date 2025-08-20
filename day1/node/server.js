b64960ed521457345a76f5e7598ead8ff442291f
honeypsg <iankur@yahoo.com>
2025-08-20 16:30:11 +0530

    added redis and s3 buckets

--- day1/node/server.js
+++ day1/node/server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+//implementing redis
+const {createClient} = require('redis');
+const {v4: uuidv4} = require('uuid');
+const redisClient = createClient();
+redisClient.connect();
+
+
+
+const S3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
 
 const multer= require('multer');
 
@@ -11,27 +28,42 @@ if(!fs.existsSync(uploadDir)){
 }
 
 //configure storage
-const storage = multer.diskStorage({
-    destination: function (req,res,cb){
-        cb(null,uploadDir);
-    },
-    filename: function (req,file,cb){
-        cb(null,Date.now()+"-" + file.originalname);
-    }
-});
+const storage = multer.memoryStorage();
 
-const upload = multer({storage:storage});
+const upload = multer({storage});
 
 
 app.get('/',(req,res)=>{
     res.send('hello world');
 })
 
-app.post('/upload', upload.single('avatar'), function (req,res,next){
+app.post('/upload', upload.single('avatar'), async function (req, res, next) {
+    const file = req.file;
     console.log(req.file);
-    res.send('Upload success, file stored in ./uploads/');
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: Date.now() + '-' + file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype
+    }
+
+    try {
+        console.log(S3);
+        const data = await S3.upload(params).promise();
+        res.json({
+            message: 'Upload success',
+            s3Url: data.Location,
+            key: data.key,
+            fileName: file.originalname,
+            size: file.size,
+            type: file.mimeType,
+            uploadedAt: new Date()
+        });
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 app.listen((8080),()=>{
-    console.log('server is running on port 800');
+    console.log('server is running on port 8080');
 });
